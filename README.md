01fba1b5f7dc6a14ab0026748b9a26e9c0811aa9
bearddan2000 <bearddan2000@yahoo.com>
2021-05-18 22:55:36 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# nodejs-cli-rabbitmq-cluster
+
+## Description
+A cluster producer/consumer
+message broker demo.
+
+## Tech stack
+- nodejs
+
+## Docker stack
+- node:latest
+- harbur/rabbitmq-cluster
+
+## To run
+`sudo ./install.sh -u`
+
+## To stop
+`sudo ./install.sh -d`
+
+## For help
+`sudo ./install.sh -h`
+
+## Credit
+- https://codefibershq.com/blog/rabbitmq-cluster-on-docker-with-nodejs-in-5-minutes
+- https://github.com/harbur/docker-rabbitmq-cluster
---
+++ consumer/bin/server.js
@@ -0,0 +1,34 @@
+let q = 'tasks';
+let amqp = require('amqp-connection-manager');
+
+function sleep(ms) {
+  if(ms <= 0){
+    return
+  }
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+let main = async () => {
+
+    var connection = amqp.connect([
+        'amqp://rabbit1:5672'
+    ]);
+
+    var channelWrapper = connection.createChannel({
+        json: true,
+        setup: function(channel) {
+          console.log("Channel created");
+            return channel.assertQueue(q, { durable: true });
+        }
+    });
+
+    channelWrapper.addSetup(function(channel) {
+        return Promise.all([
+            channel.consume(q, (msg) => {
+                console.log(msg.content.toString())
+            }, {noAck: true , exclusive: false })
+        ])
+    });
+}
+
+main();
